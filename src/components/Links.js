3dfb58611b270fcf9f86923610a12e46d901b350
Michelle <clairsin@gmail.com>
2022-05-17 09:58:44 -0400

    commit tree 5bbceebd28c4559d1af26617d788b45dac0b4609

---
+++ src/components/Links.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Links(props) {
+  return (
+    <h3 id="about">
+      Links
+      <a href = "props.links.github">https://github.com/liza</a>
+      <a href = "props.links.linkedin">https://www.linkedin.com/in/liza/</a>
+    </h3>
+  )
+}
+
+export default Links
